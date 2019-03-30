254c2d9e156391feceebce36442fb7ea8d0a7859
chaandchopra <chaandchopra@gmail.com>
2019-03-30 13:57:00 +0530

    initial commit

---
+++ slp.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdbool.h>
+int main()
+{
+    int threshold = 0; 
+    float learningParameter = 0;
+    bool incorrectClass = false;
+    while(incorrectClass = true)
+    {
+        incorrectClass = false;
+        while(1)
+        {
+            
+        }
+    }
+}
